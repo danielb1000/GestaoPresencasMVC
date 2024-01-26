1dbcbc82b374c01219beb813ad3eedbb2b591f21
danielb1000 <danyellourenco@hotmail.com>
2024-01-26 04:10:52 +0000

    Add project files.

---
+++ GestaoPresencasMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GestaoPresencasMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
